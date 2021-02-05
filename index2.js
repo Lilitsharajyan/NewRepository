3c71918e62df17f1198a47ca6a8dc983643825a8
Lilitsharajyan <71130574+Lilitsharajyan@users.noreply.github.com>
2021-02-05 22:37:16 +0400

    Create index2.js

---
+++ index2.js
@@ -0,0 +1,13 @@
+//2
+let x = 60;
+let y = 11;
+let sum = x + y;
+let dif = x - y;
+let prod = x * y;
+let quot = x / y;
+let rem = x % y;
+console.log('The sum of ' + x + ' and ' + y + ' is ' + sum);
+console.log('The differnece ' + x + ' and ' + y +  ' is ' + dif);
+console.log('The product ' + x + ' and ' + y + ' is ' + prod);
+console.log('The quotient ' + x + ' and ' + y + ' is ' + quot);
+console.log('The remainder ' + x + ' and ' + y + ' is ' + rem);
