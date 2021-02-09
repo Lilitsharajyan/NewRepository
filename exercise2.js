0301b65923d4c57a3ca408b658610c08c83dff84
Lilitsharajyan <71130574+Lilitsharajyan@users.noreply.github.com>
2021-02-09 21:09:00 +0400

    Create exercise2.js

---
+++ exercise2.js
@@ -0,0 +1,8 @@
+let number;
+if (number > 0){
+  console.log('The number is positive')
+} else if (number ===0){
+  console.log('The number is zero');
+} else { 
+  console.log('The number is negative');
+}
