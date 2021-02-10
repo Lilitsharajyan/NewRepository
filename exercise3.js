099388db160dce585db5e2fdfbf2f311724a8c3d
Lilitsharajyan <71130574+Lilitsharajyan@users.noreply.github.com>
2021-02-10 12:33:05 +0400

    Update exercise3.js

--- exercise3.js
+++ exercise3.js
@@ -1,5 +1,20 @@
+//1
 let num1 = 3;
 let num2 = 5;
 let num3 = 12;
 let largest = Math.max(num1, num2, num3);
 console.log("The largest number is " + largest);
+
+//2
+
+    let num1 = 5;
+    let num2 = 7;
+    let num3 = 8;
+		if(num1 > num2 && num1 > num3) {
+			console.log('num1 is the greatest');
+		} else if(num2 > num1 && num2 > num3) {
+			console.log('num2 is the greatest');
+		} else if(num3 > num1 && num3 > num1) {
+			console.log('num3 is the greatest');
+		}
+	
