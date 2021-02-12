a89e837899e5de00fd8a69e91fbadf79a6cae133
Lilitsharajyan <71130574+Lilitsharajyan@users.noreply.github.com>
2021-02-13 00:41:23 +0400

    Create loop6.js

---
+++ loop6.js
@@ -0,0 +1,7 @@
+  let num = 11;
+   for(let i = 2; i < num; i++)
+     if(num % i === 0){
+       console.log('The number is not prime')
+     } else if (){
+       console.log('The number is prime');
+    }
